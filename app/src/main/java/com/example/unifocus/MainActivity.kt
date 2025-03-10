1c45f10b3760db5689001ec09f78a284f4171294
Illusion <mimist-illusionard@users.noreply.github.com>
2025-03-10 14:55:33 +0300

    Добавлено:
    - Простая логика добавления задачи (без связи со структурой)

--- app/src/main/java/com/example/unifocus/MainActivity.kt
+++ app/src/main/java/com/example/unifocus/MainActivity.kt
@@ -1,13 +1,41 @@
 package com.example.unifocus
 
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+    private lateinit var taskInput: EditText
+    private lateinit var addTaskButton: Button
+    private lateinit var taskList: ListView
+    private val tasks = mutableListOf<String>()
+    private lateinit var adapter: ArrayAdapter<String>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_layout)
-        val textField = findViewById<TextView>(R.id.textView)
+
+        taskInput = findViewById(R.id.taskInput)
+        addTaskButton = findViewById(R.id.addTaskButton)
+        taskList = findViewById(R.id.taskList)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks)
+        taskList.adapter = adapter
+
+        addTaskButton.setOnClickListener {
+            val taskText = taskInput.text.toString()
+            if (taskText.isNotEmpty()) {
+                tasks.add(taskText)
+                adapter.notifyDataSetChanged()
+                taskInput.text.clear()
+            } else {
+                //Toast.makeText(this, "Введите задачу", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
